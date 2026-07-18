73d69f5b253a33a5a36c5fa0779619f0f0f3087f
NotArsal <arsalanshaikh5123@gmail.com>
2026-07-18 21:42:23 +0530

    chore: Purge Cloudflare dependencies and optimize for Vercel

--- next.config.ts
+++ next.config.ts
@@ -39,9 +39,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-if (process.env.NODE_ENV === "development") {
-  void import("@opennextjs/cloudflare").then((m) =>
-    m.initOpenNextCloudflareForDev(),
-  );
-}
